250c64b9a819a14a255f7161f5dfa09fd538cd3d
Pedro Cruz <pedrocruz@Pedros-MacBook-Pro.local>
2021-01-04 10:16:16 +0000

    start adding docs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	themeConfig: {
+		logo: '/img/vue-scichart_logo.svg',
+		sidebar: '',
+		nav: [
+			{ text: 'Home', link: '' },
+			{ text: 'Github', link: 'https://github.com/pedropcruz/vue-scichart', target: '_blank' }
+		]
+	}
+}
