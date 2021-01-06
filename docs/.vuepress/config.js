d74aeb8e8cf4f69fcda78c404229d3997dd5de41
Pedro Cruz <pedropcruz@outlook.pt>
2021-01-06 00:36:06 +0000

    docs: update component attributes and README.md

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,14 +1,27 @@
 module.exports = {
   themeConfig: {
+    title: 'vue-scichart',
+    description: 'Vue plugin for using Scichart',
     logo: '/img/vue-scichart_logo.svg',
-    sidebar: '',
-    nav: [
-      { text: 'Home', link: '/content/docs/' },
+    searchPlaceholder: 'Search...',
+    repo: 'Fidgrove/vue-scichart',
+    docsDir: 'docs/src',
+    editLinks: true,
+    repoLabel: 'Contribute',
+    lastUpdated: 'Last Updated',
+    smoothScroll: true,
+    sidebar: [
       {
-        text: 'Github',
-        link: 'https://github.com/Fidgrove/vue-scichart',
-        target: '_blank',
+        title: 'Introduction',
+        path: '/content/docs/',
+        collapsable: false,
+        children: ['/content/docs/configs'],
       },
+    ],
+    sidebarDepth: 3,
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/content/docs/' },
     ],
   },
 };
